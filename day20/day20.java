9a3a07b34b12c6fdec62ce1b97d82dc63c803f30
prathee <135495243+Prathee07@users.noreply.github.com>
2025-05-18 22:02:27 +0530

    Create day20.java

---
+++ day20/day20.java
@@ -0,0 +1,17 @@
+package practice_one;
+
+public class day20 {
+
+	public static void main(String[] args) {
+		
+		int[]arr=new int[n];         //   n.length-----------> o(1) because its a built in variable not method
+//		int [][]arr=new int[n][m];      n.length     arr[i].length
+//		int [][][]arr=new int[n][m][o];    n.length    arr[i].length     arr[i][j].length
+		System.out.println(arr.getClass());   //this will print class [I 
+		//for 2 dimension it prints class [[I
+		//for class it prints 
+		//class day20 I mean if its 
+		//day20 d=new day20(); later,,, sysout(d.getClass());
+	}
+
+}
