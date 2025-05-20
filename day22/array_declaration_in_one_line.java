13c0e12d1cb3c5342ec25fcfc553d114479b31aa
prathee <135495243+Prathee07@users.noreply.github.com>
2025-05-20 20:10:52 +0530

    Update array_declaration_in_one_line.java

--- day22/array_declaration_in_one_line.java
+++ day22/array_declaration_in_one_line.java
@@ -1,1 +1,43 @@
 
+package array;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]=new int[] {10,20,30};
+		int arr2[]= {10,20,30,40};
+		int arr3[]=new int[3];
+    int [][][]arr4={
+                {
+                   {1,2},
+                   {3,4}
+                }
+                {
+                    {6,7},
+                    {8,9}
+                }
+            };
+}
+  int [][]arr_jagged={
+    { 1,2,3},
+    {4,5,6,7}
+  };
+  int [][][]arr_jagged1=
+    {
+      {
+         {
+        1,2
+         },
+        {
+          3,4,5
+        }
+      },
+
+      
+      {
+    {
+      6,7
+    }
+      }
+  };
