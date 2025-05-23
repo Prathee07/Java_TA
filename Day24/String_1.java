af10ccae7f7ea53bee573d3187655b44893bb0e6
prathee <135495243+Prathee07@users.noreply.github.com>
2025-05-23 12:26:22 +0530

    Create String_1.java

---
+++ Day24/String_1.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class String_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=new String("JAVA");
+		String s2="CPP";
+		char []c= {'P','Y','T','H','O','N'};
+		String s3=new String(c);
+		System.out.println(s);
+		System.out.println(s2);
+		System.out.println(c);
+		System.out.println(s3);
+		
+
+	}
+
+}
+//*****************//
+OUTPUT
+JAVA
+CPP
+PYTHON
+PYTHON
